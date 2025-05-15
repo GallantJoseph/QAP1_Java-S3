954ef5a33f28077d9a25db0476f46056082fe958
GallantJoseph <gallant.joseph@outlook.com>
2025-05-15 11:08:41 -0230

    Completed Account class.

--- src/Account.java
+++ src/Account.java
@@ -5,5 +5,67 @@
  */
 
 public class Account {
+    // Private attributes definition
+    private String id;
+    private String name;
+    private int balance = 0;
 
+    // Constructors
+    // Parameterized
+    public Account(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Account(String id, String name, int balance) {
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    // Getters
+    public String getID() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    // Methods
+    // Credit an amount to the account
+    public int credit (int amount) {
+        this.balance += amount;
+        return balance;
+    }
+
+    // Debit an amount to the account
+    public int debit (int amount) {
+        if (amount <= balance) {
+            this.balance -= amount;
+        } else {
+            System.out.println("Amount exceeded balance");
+        }
+
+        return balance;
+    }
+
+    // Transfer an amount from the current account to another account
+    public int transferTo(Account anotherAccount, int amount) {
+        if (amount <= balance) {
+            this.balance -= amount;
+            anotherAccount.balance += amount;
+        }
+
+        return balance;
+    }
+
+    // toString method overload. Returns the account information in a string format.
+    public String toString() {
+        return "Account[id=" + this.id + ",name=" + this.name + ",balance=" + this.balance + "]";
+    }
 }
